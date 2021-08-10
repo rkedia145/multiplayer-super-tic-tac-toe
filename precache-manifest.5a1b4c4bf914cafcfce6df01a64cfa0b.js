88765b4fb3589a8282843ecff6922e7153405884
rkedia <raghav.kedia@flextrade.com>
2021-08-09 22:23:09 -0400

    Updates

---
+++ precache-manifest.5a1b4c4bf914cafcfce6df01a64cfa0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6fd27e78b7d4c0b03670139b18c5b9",
+    "url": "/multiplayer-super-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5b58185f6604dc85aa74",
+    "url": "/multiplayer-super-tic-tac-toe/static/css/main.1f1107aa.chunk.css"
+  },
+  {
+    "revision": "6778ea23159924999a97",
+    "url": "/multiplayer-super-tic-tac-toe/static/js/2.e9eab4eb.chunk.js"
+  },
+  {
+    "revision": "379c198c7296ef64963a14f3d7af9e55",
+    "url": "/multiplayer-super-tic-tac-toe/static/js/2.e9eab4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b58185f6604dc85aa74",
+    "url": "/multiplayer-super-tic-tac-toe/static/js/main.98c6fdb4.chunk.js"
+  },
+  {
+    "revision": "7585d7a635ceaad57f33",
+    "url": "/multiplayer-super-tic-tac-toe/static/js/runtime-main.b2505405.js"
+  },
+  {
+    "revision": "92c8c13b035e77f89a9b69588e0ba2ef",
+    "url": "/multiplayer-super-tic-tac-toe/static/media/logo2.92c8c13b.png"
+  }
+]);
